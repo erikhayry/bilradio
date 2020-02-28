ff30410f0168ffbe5e550d9afc9e618cd4a4a761
Erik Häyry <erik.hayry@svt.se>
2020-02-28 22:15:41 +0100

    notify

--- scripts/background.ts
+++ scripts/background.ts
@@ -6,19 +6,44 @@ export enum Action {
     PAUSE = 'pause'
 }
 let isPlaying = false;
-
+const buttons = [
+    {
+        "title": "Lyssna"
+    }, {
+        "title": "Stäng"
+    }
+];
 async function sendMessageToTab(tab:Tab, action: Action): Promise<any> {
     return browser.tabs.sendMessage(
         tab.id,
         {action}
     )
 }
+
+async function notify(action: Action){
+
+    browser.notifications.create({
+        "type": "basic",
+        "title": 'Profil',
+        "iconUrl": browser.runtime.getURL("icons/logo.png"),
+        "message": `Action: ${action}`,
+        //@ts-ignore
+        "buttons": buttons
+    });
+}
+
+
+
 async function sendMessageToContent(action: Action): Promise<any>{
     console.log("Action", action)
+    setTimeout(() => {
+        notify(action);
+    }, 5000)
     try {
         const tabs = await browser.tabs.query({});
         const messages = tabs.map((tab) => {
             console.log("tab", tab.mutedInfo)
+
             switch (action) {
                 case 'pause':
                     browser.tabs.update(tab.id, {"muted": true});
@@ -39,7 +64,12 @@ async function sendMessageToContent(action: Action): Promise<any>{
     }
 }
 
-console.log("browser", browser)
+browser.notifications.onButtonClicked.addListener((id, index) => {
+    //browser.notifications.clear(id);
+    console.log("You chose: " + buttons[index].title);
+    browser.tabs.create({ url: "options.html" });
+});
+
 browser.browserAction.onClicked.addListener(async () => {
     const action: Action = isPlaying ? Action.PAUSE : Action.RESUME;
     isPlaying  = !isPlaying;
