145fc5c5440e098436ab1f2303e5153478b05228
Erik Häyry <erik.hayry@svt.se>
2020-02-28 23:03:05 +0100

    api call

--- scripts/background.ts
+++ scripts/background.ts
@@ -1,6 +1,18 @@
 import {browser, Tabs} from "webextension-polyfill-ts";
 import Tab = Tabs.Tab;
 
+const EkotId = 4540;
+const P1Id = 132;
+
+fetch('http://api.sr.se/api/v2/scheduledepisodes?channelid=132&format=json')
+    .then((response) => {
+        return response.json();
+    })
+    .then(({schedule}) => {
+        const nextEpisode = schedule.find(({program}: any) => program.id === EkotId)
+        console.log('nextEpisode', nextEpisode);
+    });
+
 export enum Action {
     RESUME = 'resume',
     PAUSE = 'pause'
@@ -8,9 +20,8 @@ export enum Action {
 let isPlaying = false;
 const buttons = [
     {
-        "title": "Lyssna"
-    }, {
-        "title": "Stäng"
+        "title": "Lyssna",
+        "iconUrl": browser.runtime.getURL("icons/logo.png"),
     }
 ];
 async function sendMessageToTab(tab:Tab, action: Action): Promise<any> {
@@ -21,64 +32,52 @@ async function sendMessageToTab(tab:Tab, action: Action): Promise<any> {
 }
 
 async function notify(action: Action){
-
     browser.notifications.create({
-        "type": "basic",
-        "title": 'Profil',
-        "iconUrl": browser.runtime.getURL("icons/logo.png"),
-        "message": `Action: ${action}`,
+        type: "basic",
+        title: 'Profil',
+        iconUrl: browser.runtime.getURL("icons/logo.png"),
+        message: `Action: ${action}`,
         //@ts-ignore
-        "buttons": buttons
+        buttons: buttons,
+        requireInteraction: true
     });
 }
 
-
-
 async function sendMessageToContent(action: Action): Promise<any>{
     console.log("Action", action)
-    setTimeout(() => {
-        notify(action);
-    }, 5000)
-    try {
-        const tabs = await browser.tabs.query({});
-        const messages = tabs.map((tab) => {
-            console.log("tab", tab.mutedInfo)
+    const tabs = await browser.tabs.query({});
+    const messages = tabs.map((tab) => {
+        console.log("tab", tab.mutedInfo)
 
-            switch (action) {
-                case 'pause':
-                    browser.tabs.update(tab.id, {"muted": true});
-                    break;
-                case 'resume':
-                    browser.tabs.update(tab.id, {"muted": false});
-                    break;
-                default:
-                    console.log("Unknown action: ", action);
-            }
-            return sendMessageToTab(tab, action);
-        });
+        switch (action) {
+            case 'pause':
+                browser.tabs.update(tab.id, {"muted": true});
+                break;
+            case 'resume':
+                browser.tabs.update(tab.id, {"muted": false});
+                break;
+            default:
+                console.log("Unknown action: ", action);
+        }
+        return sendMessageToTab(tab, action);
+    });
 
-        return Promise.all(messages)
-    } catch(error){
-        console.error(error);
-        return Promise.reject(error)
-    }
+    return Promise.all(messages)
 }
 
-browser.notifications.onButtonClicked.addListener((id, index) => {
-    //browser.notifications.clear(id);
-    console.log("You chose: " + buttons[index].title);
+browser.notifications.onButtonClicked.addListener(async (id, index) => {
+    try {
+        await sendMessageToContent(Action.PAUSE);
+    } catch(error){
+        console.error(error);
+    }
     browser.tabs.create({ url: "options.html" });
 });
 
 browser.browserAction.onClicked.addListener(async () => {
     const action: Action = isPlaying ? Action.PAUSE : Action.RESUME;
-    isPlaying  = !isPlaying;
-
-    sendMessageToContent(action)
-        .then(res => {
-            console.log("response", res)
-        })
-        .catch(({message}) => {
-            console.error(message)
-        })
+    isPlaying  = true;
+    setTimeout(() => {
+        notify(action);
+    }, 0)
 });
